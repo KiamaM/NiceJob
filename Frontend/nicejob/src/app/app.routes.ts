6c3e2e243317b16da394bc35ddb4e097d24eb317
KiamaM <muriithikiamad1@gmail.com>
2024-03-13 03:18:20 +0300

    Landing Page

--- Frontend/nicejob/src/app/app.routes.ts
+++ Frontend/nicejob/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RegisterFormComponent } from './Components/register-form/register-form.component';
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'register', component:RegisterFormComponent},
+    {path: '', component:LandingPageComponent},
+    
+];
